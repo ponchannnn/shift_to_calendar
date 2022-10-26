0d6733e60632c8c4084a56d2d7b1b81a1eab14c0
ponCHANNN <mizuki.7302@gmail.com>
2022-10-26 10:54:50 +0900

    First commit

---
+++ createEventinCalendar.js
@@ -0,0 +1,43 @@
+const Calendar = require('node-google-calendar');
+const config = require('./calendarConfig.js');
+const calId = config.calendarId;
+
+
+module.exports = {
+    create:create
+}
+
+let event = {
+    'start': {'dateTime': '2019-08-29T10:00:00+09:00'},
+    'end'  : {'dateTime': '2019-08-29T11:00:00+09:00'},
+    // ------------
+    // 終日イベントの場合
+    // 'start': {'date': '2019-08-30'},
+    // 'end'  : {'date': '2019-08-30'},
+    // ------------
+    'summary': 'カレンダーイベントのタイトル',
+    'description': 'カレンダーイベントの説明欄に表示する文章',
+    // カレンダーで表示するイベントの色
+    'colorId': 1, 
+    // 1:ラベンダ(薄紫)
+    // 2:セージ(薄緑)
+    // 3:ブドウ(濃紫)
+    // 4:フラミンゴ(ピンク)
+    // 5:バナナ
+    // 6:みかん
+    // 7:ピーコック(水色)
+    // 8:グラファイト(灰色)
+    // 9:ブルーベリー(濃青)
+    // 10:バジル(濃緑)
+    // 11:トマト
+  };
+
+function create () {
+    cal.Events.insert(calId, event)
+.then(resp => {
+  console.log(resp);
+})
+.catch(err => {
+  console.log(err.message);
+});
+}
